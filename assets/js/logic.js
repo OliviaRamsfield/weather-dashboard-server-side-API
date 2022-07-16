fbd91efd376faa8d100e741bc228fb67aec01798
Olivia <ramsfol@gmail.com>
2022-07-16 09:13:15 -0500

    current weather displays

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,6 +1,7 @@
-var cityInputEl = document.querySelector("#city");
+var cityInputEl = document.querySelector("#city")
 var cityContainerEl = document.querySelector("#user-form")
 var city = cityInputEl.value.trim()
+var currentWeatherContainerEl = document.querySelector("#city-weather-container")
 
 var formSubmitHandler = function(event) {
     //prevent page from refreshing
@@ -44,15 +45,57 @@ function getLatLon () {
 }
 
 var getWeather = function (lat, lon) {
-    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&limit=5&appid=41a56cf98d8a606201c73d9d3aa3cd7f"
+    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&limit=5&units=imperial&appid=41a56cf98d8a606201c73d9d3aa3cd7f"
     console.log("This is my" + apiUrl);
     fetch(apiUrl).then(function(response) {
         if (response.ok) {
             response.json().then(function(data) {
+                console.log(data);
+                //gathering current weather information
                 const temp = data.current.temp;
-                console.log(`this is my temp ${temp}`);})
+                console.log(`this is the temp ${temp}`);
+                const wind = data.current.wind_speed;
+                console.log(`this is the wind ${wind}`);
+                const humidity = data.current.humidity;
+                console.log(`this is the humidity ${humidity}`);
+                const uvIndex = data.current.uvi;
+                console.log(`this is the UV Index ${uvIndex}`);
+
+                displayCurrentWeather(temp, wind, humidity, uvIndex);
+                
+                //gathering 5-day forecast information
+                const dayOneHumid = data.daily[1].humidity;
+                console.log(`this is the humidity ${dayOneHumid}`);
+                const dayOneTemp = data.daily[1].temp.day;
+                console.log(`this is the temp ${dayOneTemp}`);
+                const dayOneWind = data.daily[1].wind_speed;
+                console.log(`this is the wind ${dayOneWind}`);
+            })
         }
     })
+}
+
+var displayCurrentWeather = function (temp, wind, humidity, uvIndex) {
+    //check if API returned any weather
+    if (currentWeatherContainerEl.length === 0) {
+        currentWeatherContainerEl.textContent = "No Weather Available"
+        return;
+    }
+    //clear old content
+    currentWeatherContainerEl.textContent = ""
+    //format weather information
+    var weatherTempEl = document.createElement("li")
+    weatherTempEl.textContent = "Temp: " + temp + "°F"
+    currentWeatherContainerEl.appendChild(weatherTempEl)
+    var weatherWindEl = document.createElement("li")
+    weatherWindEl.textContent = "Wind: " + wind + " MPH"
+    currentWeatherContainerEl.appendChild(weatherWindEl)
+    var weatherHumidityEl = document.createElement("li")
+    weatherHumidityEl.textContent = "Humidity: " + humidity + "%"
+    currentWeatherContainerEl.appendChild(weatherHumidityEl)
+    var weatherUviEl = document.createElement("li")
+    weatherUviEl.textContent = "UV Index: " + uvIndex
+    currentWeatherContainerEl.appendChild(weatherUviEl)
 }
 
 //add event listeners to form container
